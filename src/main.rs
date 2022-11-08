d72ec45fbd9e73cb133d0600e88a943e7c4ac6b8
DeveloperC <DeveloperC@protonmail.com>
2022-11-08 21:06:20 +0000

    feat: adding logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum ConsistentWhitespaceError {
+    #[error("The current working directory's can not be expressed as a String.")]
+    NoWorkingDirectoryString,
+}
+
+const ERROR_EXIT_CODE: i32 = 1;
+
 fn main() {
-    match std::env::current_dir() {
-        Ok(current_directory) => {
-            println!("{:?}", current_directory.to_str());
+    pretty_env_logger::init();
+    trace!("Version {}.", env!("CARGO_PKG_VERSION"));
+
+    match get_current_working_directory() {
+        Ok(current_working_directory) => {}
+        Err(error) => {
+            error!("Unable to get the current working directory, got the error {error:?}.");
+            std::process::exit(ERROR_EXIT_CODE);
         }
-        Err(_) => {}
+    }
+}
+
+fn get_current_working_directory() -> Result<String, Box<dyn std::error::Error>> {
+    match std::env::current_dir()?.to_str() {
+        Some(current_working_directory) => Ok(current_working_directory.to_string()),
+        None => Err(Box::new(
+            ConsistentWhitespaceError::NoWorkingDirectoryString,
+        )),
     }
 }
