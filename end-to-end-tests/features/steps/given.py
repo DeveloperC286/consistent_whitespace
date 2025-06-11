9e41e1e07fa02c222a81e3aafcd9070011cbded2
DeveloperC <DeveloperC@protonmail.com>
2025-06-11 11:32:47 +0100

    build: replacing Earthly with Makefile & Docker build/run

--- end-to-end-tests/features/steps/given.py
+++ end-to-end-tests/features/steps/given.py
@@ -9,4 +9,4 @@ from behave import given
 def empty_directory(context):
     context.behave_directory = os.getcwd()
     context.execution_directory = tempfile.mkdtemp()
-    context.consistent_whitespace_path = f"{context.behave_directory}/../target/debug/consistent_whitespace"  # fmt: off
+    context.consistent_whitespace_path = f"{context.behave_directory}/../target/x86_64-unknown-linux-musl/debug/consistent_whitespace" # fmt: off
