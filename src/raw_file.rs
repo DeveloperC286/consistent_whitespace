e1fe14178a56ba99777ff76220f0e5cbf253c323
DeveloperC <DeveloperC@protonmail.com>
2025-06-15 01:50:09 +0100

    fix: correcting file path walking (#202)

--- src/raw_file.rs
+++ src/raw_file.rs
@@ -16,34 +16,42 @@ pub struct RawLine {
     pub line_number: usize,
 }
 
-pub fn get_raw_files(paths: &[PathBuf]) -> Result<RawFiles> {
+pub fn get_raw_files(search: &[PathBuf]) -> Result<RawFiles> {
+    let files = get_files_paths(search)?;
+    get_raw_files_from_paths(files)
+}
+
+fn get_files_paths(search: &[PathBuf]) -> Result<Vec<PathBuf>> {
     let mut files = Vec::new();
-    for path in paths {
-        if path.is_file() {
-            files.push(get_raw_file(path)?);
-        } else {
-            files.extend(get_raw_files_in_directory(path)?);
-        }
+    for path in search {
+        files.extend(get_file_paths(path.as_path())?);
     }
+
+    files.sort();
+    files.dedup();
     Ok(files)
 }
 
-fn get_raw_files_in_directory(directory: &Path) -> Result<RawFiles> {
-    let mut files = Vec::new();
+fn get_file_paths(search: &Path) -> Result<Vec<PathBuf>> {
+    let mut paths = Vec::new();
 
-    for entry in Walk::new(directory) {
+    for entry in Walk::new(search) {
         let entry = entry?;
         let path = entry.path();
 
-        if path != directory {
-            if path.is_file() {
-                files.push(get_raw_file(path)?);
-            } else {
-                files.extend(get_raw_files_in_directory(path)?);
-            }
+        if path.is_file() {
+            paths.push(path.to_path_buf());
         }
     }
 
+    Ok(paths)
+}
+
+fn get_raw_files_from_paths(paths: Vec<PathBuf>) -> Result<RawFiles> {
+    let mut files = Vec::new();
+    for path in paths {
+        files.push(get_raw_file(&path)?);
+    }
     Ok(files)
 }
 
